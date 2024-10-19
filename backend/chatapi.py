c496f3f70c6caa35f01ed9a3b66a43266f75adbe
Keita Nakata <keita.prog823@gmail.com>
2024-10-19 13:30:53 +0900

    chatapiを使用するエンドポイントを作成

--- backend/chatapi.py
+++ backend/chatapi.py
@@ -1,8 +1,10 @@
-# %%
 import base64
 from dotenv import load_dotenv
 from openai import OpenAI
 import  os
+import sys
+print(sys.executable)
+
 
 load_dotenv()
 
@@ -15,27 +17,39 @@ def encode_image(image_path):
 # text: 入力に使うテキスト（音声で取得した部分）
 # base64_image: スクショ画像をbase64エンコードしたもの
 # max_tokens: 生成するテキストの最大トークン数
-def chatapi(input_text, base64_image, max_tokens=300):
+def chatapi(input_text, base64_image = None, max_tokens = 300):
    # OpenAI APIのクライアントを作成する.
     client = OpenAI(
         api_key=os.getenv("OPENAI_API_KEY"),
     )
-
-    # チャットの応答を生成する
-    response = client.chat.completions.create(
-        # model の名前は gpt-4-vision-previewが使えなくなったので、以下に変更.
-        model="gpt-4o-mini",
-        messages=[
-            {
-                "role": "user",
-                "content": [
-                    {"type": "text", "text": "この画面の下の方に映っているエラーの原因を教えて下さい。"},  # ここに質問を書く
-                    {"type": "image_url", "image_url":{"url": f"data:image/jpeg;base64,{base64_image}"}},  # 画像の指定の仕方がちょい複雑
-                ],
-            }
-        ],
-        max_tokens=300,
-    )
+    
+    if (base64_image == None):
+        # チャットの応答を生成する
+        response = client.chat.completions.create(
+            model="gpt-4o-mini",
+            messages=[
+                {
+                    "role": "user",
+                    "content": input_text,
+                }
+            ],
+            max_tokens=max_tokens,
+        )
+    else:
+        # チャットの応答を生成する
+        response = client.chat.completions.create(
+            model="gpt-4o-mini",
+            messages=[
+                {
+                    "role": "user",
+                    "content": [
+                        {"type": "text", "text": input_text},  # ここに質問を書く
+                        {"type": "image_url", "image_url":{"url": f"data:image/jpeg;base64,{base64_image}"}},  # 画像の指定の仕方がちょい複雑
+                    ],
+                }
+            ],
+            max_tokens=300,
+        )
     
     return response.choices[0]
 
@@ -47,6 +61,6 @@ image_path = "image/codeimage.png"
 base64_image = encode_image(image_path)
 
 # チャットの応答を生成する
-response = chatapi("この画面の下の方に映っているエラーの原因を教えて下さい。", base64_image)
+# response = chatapi("この画面の下の方に映っているエラーの原因を教えて下さい。", base64_image)
 
-print(response)
+# print(response)
